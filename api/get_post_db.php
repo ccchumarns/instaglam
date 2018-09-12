f0f0b2c3ddf77b8c7d736fcf191b7bfb6f4a1632
EC2 Default User <ec2-user@ip-172-31-30-251.ap-southeast-1.compute.internal>
2018-09-12 03:03:18 +0000

    mod get_post_db.php

--- api/get_post_db.php
+++ api/get_post_db.php
@@ -4,22 +4,33 @@
 include_once('common.php');
 $db = new DB();
 
- 
+$id = 1;
+
+$sql = 'SELECT post.post_id, user.name, post.title, post.img_url, post.price, 
+coupon.discount, category.category_name, post.description, post.latitude, 
+post.longitude, post.go_sum
+FROM `post`
+JOIN `user` ON post.user_id = user.user_id
+JOIN `category` ON post.category_id = category.category_id
+JOIN `coupon` ON post.coupon_id = coupon.coupon_id 
+WHERE post.post_id = '.$id;
+$dataset = $db->select($sql);
+
+$data = $dataset[0];
+
 $a = [
-  'post_id'=> 1,
-  'category'=> 'フード',
-  'title'=> 'サイゼリヤ',
-  'user_name'=> 'test',
-  'img_url'=> 'https::imgfp.hotp.jp/IMGH/23/13/P027762313/P027762313_480.jpg',
-  'description'=> 'とっても美味しかった',
-  'coupon_discount'=> '今なら半額！',
+  'post_id'=> $data['post_id'],
+  'category'=> $data['category_name'],
+  'title'=> $data['title'],
+  'user_name'=> $data['name'],
+  'img_url'=> $data['img_url'],
+  'description'=> $data['description'],
+  'coupon_discount'=> $data['discount'],
   'address'=> 'Japan',
-  'go_sum'=> 200,
-  'price'=> 100,
-  'latitude'=> 35.360556,
-  'longitude'=> 138.727778
+  'go_sum'=> $data['go_sum'],
+  'price'=> $data['price'],
+  'latitude'=> $data['latitude'],
+  'longitude'=> $data['longitude']
 ];
-
-
 
 Response::send($a);
